db22cdd21d28de01e460108c2b643c35ea15519c
WvDeldenHR <0960572@hr.nl>
2019-03-20 12:54:31 +0100

    Initial commit

--- app/src/main/java/com/example/projectb/MainActivity.java
+++ app/src/main/java/com/example/projectb/MainActivity.java
@@ -17,14 +17,17 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigationHome:
+                    mTextMessage.setText(R.string.titleHome);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigationNews:
+                    mTextMessage.setText(R.string.titleNews);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigationCaledar:
+                    mTextMessage.setText(R.string.titleCalendar);
+                    return true;
+                case R.id.navigationMaps:
+                    mTextMessage.setText(R.string.titleMaps);
                     return true;
             }
             return false;
